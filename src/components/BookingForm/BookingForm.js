db1c7b875aafee0cace383efbe31a1da9c99ab98
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-11-23 23:05:11 +0800

    Login page is now responsive

--- src/components/BookingForm/BookingForm.js
+++ src/components/BookingForm/BookingForm.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { UserContext } from '../../App';
 
 const BookingForm = ({ placeId, originName, name }) => {
 	const [loggedInUser, setLoggedInUser, booking, setBooking] = useContext(UserContext);
+	let history = useHistory();
 
 	const { register, handleSubmit, errors } = useForm();
 	const onSubmit = (data) => {
@@ -18,57 +19,30 @@ const BookingForm = ({ placeId, originName, name }) => {
 			to: to,
 		};
 		setBooking(getBookingInfo);
+		history.push('/bookingPlace');
 	};
 	return (
 		<div className="booking-form">
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<label htmlFor="origin">Origin</label>
-				<input
-					name="originName"
-					defaultValue={originName}
-					ref={register}
-					className="form-control"
-				/>
+				<input name="originName" defaultValue={originName} ref={register} className="form-control"/>
+
 				<label htmlFor="destination">Destination</label>
-				<input
-					name="destination"
-					defaultValue={name}
-					ref={register({ required: true })}
-					className="form-control"
-				/>
+				<input name="destination" defaultValue={name} ref={register({ required: true })} className="form-control"/>
 				{errors.destination && <span>This field is required</span>}
+
 				{/* DATE PICKER */}
 				<div className="date-picker-wrap">
 					<div className="date-picker">
 						<label>From</label>
-						<input
-							type="date"
-							name="from"
-							defaultValue="Cox's Bazar"
-							ref={register({ required: true })}
-						/>
+						<input type="date" name="from" defaultValue="Cox's Bazar" ref={register({ required: true })}/>
 					</div>
 					<div className="date-picker">
 						<label>To</label>
-						<input
-							type="date"
-							name="to"
-							defaultValue="Cox's Bazar"
-							ref={register({ required: true })}
-						/>
+						<input type="date" name="to" defaultValue="Cox's Bazar" ref={register({ required: true })}/>
 					</div>
 				</div>
-				{booking.isBooking ? (
-					<Link to="/bookingPlace">
-						<button className="btn travel-btn">Travel your place</button>
-					</Link>
-				) : (
-					<input
-						type="submit"
-						value="Start Booking"
-						className="form-control booking-btn"
-					/>
-				)}
+				<input type="submit"value="Start Booking"className="form-control booking-btn"/>
 			</form>
 		</div>
 	);
