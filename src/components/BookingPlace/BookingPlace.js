1b1e7780853a070d3f17fc55c12e4e7f227921b9
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-11-24 11:19:19 +0800

    Travel guru is now fully responsive

--- src/components/BookingPlace/BookingPlace.js
+++ src/components/BookingPlace/BookingPlace.js
@@ -24,51 +24,67 @@ const ShowPlace = () => {
         name = selectedPlace.name;
 		latitude = selectedPlace.lat;
         longitude = selectedPlace.lng;
-        
-	}
-
+    }
     return (
         <div className="container">
         {booking.isBooking ? 
             <div>
                 <div className="border-top">
-                <p>252 stays {booking.from} - {booking.to} 3 guests</p>
-                <h4><strong>Stay in {name}</strong></h4>
+                <p className="mt-3">252 stays {booking.from} - {booking.to} 3 guests</p>
+                <h4 className="my-4"><strong>Stay in {name}</strong></h4>
                 </div>
                 <div className="row">
-                    <div className="col-md-6">
-                        <div className="d-flex justify-content-center mt-4">
-                            <img className="w-50  booking-place-image" src={selectedPlace && selectedPlace.description.roomeONe.img} alt=""/>
-                            <div>
-                                <h5>{selectedPlace && selectedPlace.description.roomeONe.title}</h5>
-                                <p className="text-secondary">4 guests 2 bedrooms 2 beds 2 baths</p>
-                                <p className="text-secondary">Wife Air conditioning kitchen</p>
-                                <p className="text-secondary">Cancellation fexibility available</p>
-                                <p><GradeIcon /> 4.9 (20) $34/night $167 total</p>
+                    <div className="col-md-7">
+                        <div className="card mb-3">
+                            <div className="row no-gutters">
+                                <div className="col-md-6">
+                                <img src={selectedPlace && selectedPlace.description.roomeONe.img} className="card-img img-fluid" alt="..."/>
+                                </div>
+                                <div className="col-md-6">
+                                <div className="card-body">
+                                    <h5 className="card-title font-weight-normal">{selectedPlace && selectedPlace.description.roomeONe.title}</h5>
+                                    <p className="card-text">4 guests 2 bedrooms 2 beds 2 baths</p>
+                                    <p className="card-text">Wife Air conditioning kitchen</p>
+                                    <p className="card-text">Cancellation fexibility available</p>
+                                    <p className="card-text"><GradeIcon /> 4.9 (20) $34/night $167 total</p>
+                                </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="d-flex justify-content-center mt-4">
-                            <img className="w-50  booking-place-image" src={selectedPlace && selectedPlace.description.roomeTwo.img} alt=""/>
-                            <div>
-                                <h5>{selectedPlace && selectedPlace.description.roomeTwo.title}</h5>
-                                <p className="text-secondary">4 guests 2 bedrooms 2 beds 2 baths</p>
-                                <p className="text-secondary">Wife Air conditioning kitchen</p>
-                                <p className="text-secondary">Cancellation fexibility available</p>
-                                <p><GradeIcon /> 4.9 (20) $34/night $167 total</p>
+                        <div className="card mb-3">
+                            <div className="row no-gutters">
+                                <div className="col-md-6">
+                                <img src={selectedPlace && selectedPlace.description.roomeTwo.img} className="card-img img-fluid" alt="..."/>
+                                </div>
+                                <div className="col-md-6">
+                                <div className="card-body">
+                                    <h5 className="card-title font-weight-normal">{selectedPlace && selectedPlace.description.roomeTwo.title}</h5>
+                                    <p className="card-text">4 guests 2 bedrooms 2 beds 2 baths</p>
+                                    <p className="card-text">Wife Air conditioning kitchen</p>
+                                    <p className="card-text">Cancellation fexibility available</p>
+                                    <p className="card-text"><GradeIcon /> 4.9 (20) $34/night $167 total</p>
+                                </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="d-flex justify-content-center mt-4">
-                            <img className="w-50  booking-place-image" src={selectedPlace && selectedPlace.description.roomeThree.img} alt=""/>
-                            <div>
-                                <h5>{selectedPlace && selectedPlace.description.roomeThree.title}</h5>
-                                <p className="text-secondary">4 guests 2 bedrooms 2 beds 2 baths</p>
-                                <p className="text-secondary">Wife Air conditioning kitchen</p>
-                                <p className="text-secondary">Cancellation fexibility available</p>
-                                <p><GradeIcon /> 4.9 (20) $34/night $167 total</p>
+                        <div className="card mb-3">
+                            <div className="row no-gutters">
+                                <div className="col-md-6">
+                                <img src={selectedPlace && selectedPlace.description.roomeThree.img} className="card-img img-fluid" alt="..."/>
+                                </div>
+                                <div className="col-md-6">
+                                <div className="card-body">
+                                    <h5 className="card-title font-weight-normal">{selectedPlace && selectedPlace.description.roomeThree.title}</h5>
+                                    <p className="card-text">4 guests 2 bedrooms 2 beds 2 baths</p>
+                                    <p className="card-text">Wife Air conditioning kitchen</p>
+                                    <p className="card-text">Cancellation fexibility available</p>
+                                    <p className="card-text"><GradeIcon /> 4.9 (20) $34/night $167 total</p>
+                                </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-6 d-block">
+                    <div className="col-md-5 google-map">
                         <GoogleMap lat={latitude} lng={longitude}></GoogleMap>
                     </div>
                 </div>
@@ -79,7 +95,7 @@ const ShowPlace = () => {
                 <button className="btn btn-warning"><ArrowBackIcon /> to Booking</button>
              </Link>
              </div>
-            }
+        }
         </div>
     );
 };
